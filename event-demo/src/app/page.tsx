4e7d7dde4f9140d99de384be2bdce48cf9ba21dc
BuildTools <unconfigured@null.spigotmc.org>
2024-01-26 14:19:27 +0000

    - Adds Auth modal front

--- event-demo/src/app/page.tsx
+++ event-demo/src/app/page.tsx
@@ -8,35 +8,36 @@ import { Video, Chat, Stream } from "@/components/Stream";
 import { Controls } from "@/components/Controls";
 import { useEffect, useState } from "react";
 import { Login, LoginTitle } from "@/components/Login";
+import { AuthModal } from "@/components/AuthModal";
+import * as jwt from "jsonwebtoken";
 
 export default function Home() {
   const [username, setUsername] = useState('');
+  const [token, setToken] = useState('');
   const [client, setClient] = useState(false)
 
   useEffect(() => setClient(true), [])
-  
-  const onAPI = (api: WidgetBot['api']) => {
-    api.on("signIn", user => {
-      setUsername(user.username);
-    });
 
-    api.on("alreadySignedIn", user => {
-      setUsername(user.username);
-    });
-
-    api.on("signOut", () => {
-      setUsername('');
-    })
+  const auth = (username: string) => {
+    if (username == null) return;
+    // DO authentication stuff here and return JWT from platform, then:
+    var token = jwt.sign({data: username}, "superSecretKey");
+    setUsername(username);
+    setToken(token)
   }
 
   return (
     <Container color="primary">
       <Header />
       <Stream>
+        <AuthModal
+          token={token}
+          auth={auth}
+        ></AuthModal>
         <Video>
           {!username ?
             <Login>
-              <LoginTitle>Log in to chat to access the stream!</LoginTitle>
+              <LoginTitle>Log in to access the stream!</LoginTitle>
             </Login>
           : <iframe
               width="100%"
@@ -56,8 +57,9 @@ export default function Home() {
             server="299881420891881473"
             channel="355719584830980096"
             shard="https://emerald.widgetbot.io"
-            onAPI={onAPI}
-          />}
+            token={token}
+          />
+          }
         </Chat>
       </Stream>
       {username && <Controls username={username} />}
---
+++ event-demo/src/components/AuthModal.tsx
@@ -0,0 +1,86 @@
+import { styled, theme } from "@/stitches/stitches.config"
+import { Dispatch, SetStateAction, useState } from "react"
+
+const ModalWrapper = styled("div", {
+    position: "absolute",
+    width: "stretch",
+    height: "stretch",
+    background: "rgba(0, 0, 0, 0.7)",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+})
+
+const Modal = styled("div", {
+    position: "relative",
+    background: "#222",
+    width: "40vw",
+    // height: "40vh",
+    borderRadius: "1vh",
+    display: "flex",
+    minWidth: "300px",
+    paddingInline: "20px",
+    paddingBlock: "4vh",
+    justifyContent: "center",
+    gap: "4vh",
+    alignItems: "left",
+    flexDirection: "column",
+    fontSize: "6ch",
+    boxShadow: "0px 0px 100px teal",
+    userSelect: "none"
+})
+
+const Username = styled("input", {
+    position: "relative",
+    fontSize: "inherit",
+    padding: "10px",
+    borderRadius: "10px",
+    outline: "none",
+    border: "none",
+})
+
+const Submit = styled("button", {
+    background: "#222",
+    alignSelf: "end",
+    outline: "none",
+    border: "none",
+    fontSize: "inherit",
+    cursor: "pointer"
+})
+
+type A = {
+    token: string
+    auth: (username: string) => void
+}
+
+export const AuthModal: React.FC<A> = ({token, auth}) => {
+
+    const [value, setValue] = useState<string>('')
+
+    const handleChange = (e: Event) => {
+        setValue((e.currentTarget as HTMLInputElement).value)
+    }
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if (e.code === "Enter") auth(value)
+    }
+
+    return (token === "" &&
+        <ModalWrapper>
+            <Modal>
+                    Enter your chat username:
+                    <Username 
+                        onChange={handleChange}
+                        onKeyDown={handleKeyDown}
+                        type={"text"} 
+                        spellcheck={false}
+                    />
+                    <Submit
+                        onClick={() => auth(value)}
+                    >
+                        Log in 
+                    </Submit>
+            </Modal>
+        </ModalWrapper>
+    )
+}
